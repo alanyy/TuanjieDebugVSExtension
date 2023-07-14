31e7625d87f424208e09584792bff37e49e622f6
alan.yao <alan.yao@unity.cn>
2023-07-14 16:12:48 +0800

    add debug command

---
+++ TuanjieDebugVSExtension/Commands/AttachDebuggerCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace TuanjieDebugVSExtension
+{
+    [Command(PackageIds.CmdIdAttachAndDebug)]
+    internal sealed class AttachDebuggerCommand : BaseCommand<AttachDebuggerCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            await VS.MessageBox.ShowWarningAsync("AttachDebuggerCommand", "Button clicked");
+        }
+    }
+}
